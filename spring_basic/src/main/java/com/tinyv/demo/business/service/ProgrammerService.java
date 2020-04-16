c06dbaedf82bdbc032ac89c276a4ebe412f79941
mayue <374176008@qq.com>
2020-04-16 18:48:41 +0800

    1. 创建新的module spring_basic. 并将现有代码移入该module

---
+++ spring_basic/src/main/java/com/tinyv/demo/business/service/ProgrammerService.java
@@ -0,0 +1,26 @@
+package com.tinyv.demo.business.service;
+
+import com.tinyv.demo.business.bean.Programmer;
+
+import java.util.List;
+
+/**
+ *
+ * @author TinyV
+ * @date 2019/11/19
+ */
+public interface ProgrammerService {
+
+    /**
+     * 查询所有的programmer
+     * @return
+     */
+    List<Programmer> listProgrammers();
+
+    /**
+     * 插入Programmer
+     * @param programmer
+     */
+    void insertProgrammer(Programmer programmer);
+
+}
