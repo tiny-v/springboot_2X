064bb742dd8470eb5dc35c34ecf8e29a1c565a12
mayue <mayue@huayun.com>
2022-01-06 18:00:21 +0800

    1. 移除RocketMQ内容

---
+++ spring_basic/src/main/java/com/tinyv/demo/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,39 @@
+package com.tinyv.demo.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author tiny_v
+ * @date 2022/1/6.
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    private static final String TOPIC_STOCK = "STOCK_TOPIC";
+
+    private static final String QUEUE_STOCK = "STOCK_QUEUE";
+
+    private static final String ROUTER_STOCK = "stock.*";
+
+    @Bean
+    public TopicExchange stockTopic(){
+        return new TopicExchange(TOPIC_STOCK, true, false);
+    }
+
+    @Bean
+    public Queue stockQueue(){
+        return new Queue(QUEUE_STOCK, true);
+    }
+
+    @Bean
+    public Binding bindStockQueueToTopic(@Qualifier("stockQueue") Queue stockQueue, @Qualifier("stockTopic") TopicExchange stockTopic){
+        return BindingBuilder.bind(stockQueue).to(stockTopic).with(ROUTER_STOCK);
+    }
+
+}
